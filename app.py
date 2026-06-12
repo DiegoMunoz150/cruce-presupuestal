6e4a8aec9b6751d993a6db28b0e56ae7c66b2b48
JCaceres-R <177897458+JCaceres-R@users.noreply.github.com>
2026-06-12 18:32:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from motor_excel import generar_cruce
 import os
 
 # ======================
-# APP Permisos y Configuración Prueba test
+# APP Permisos y Configuración Prueba test_2
 # ======================
 app = Flask(__name__)
 
